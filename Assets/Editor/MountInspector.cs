9063e877cd96e83661f5f4b51336aaa65ad20204
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-15 21:57:59 +0200

    - Adding MountInspector.cs for modifying Mount/Heart class on Editor

---
+++ Assets/Editor/MountInspector.cs
@@ -0,0 +1,61 @@
+﻿using Items;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[CustomEditor(typeof(Mount), true)]
+public class MountInspector : Editor{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        var myTarget = (Mount) target;
+
+        myTarget.id = (ushort) Mathf.Clamp(EditorGUILayout.IntField("ID",myTarget.id), 0, ushort.MaxValue);
+        myTarget.typeOfMount = (Mount.Type) EditorGUILayout.EnumPopup("Type Of Mount", myTarget.typeOfMount);
+        if (myTarget.typeOfMount != Mount.Type.Dynamic) return;
+        myTarget.optionsOfMount = (Mount.Options) EditorGUILayout.EnumPopup("Options", myTarget.optionsOfMount);
+        switch (myTarget.optionsOfMount)
+        {
+            case Mount.Options.VelocityUpgrader:
+                myTarget.velocitySpeed = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Velocity Speed",myTarget.velocitySpeed), 0, ushort.MaxValue);
+                break;
+            case Mount.Options.Jetpack:
+                myTarget.jetpackForce = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Jetpack Speed",myTarget.jetpackForce), 0, ushort.MaxValue);
+                myTarget.jetpackAmount = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Jetpack Amount",myTarget.jetpackAmount), 0, ushort.MaxValue);
+                break;
+            
+
+        }
+        serializedObject.ApplyModifiedProperties();
+        if (!GUI.changed) return;
+        EditorUtility.SetDirty(myTarget);
+        EditorSceneManager.MarkSceneDirty(myTarget.gameObject.scene);
+        
+    }
+} 
+
+[CustomEditor(typeof(Heart), true)]
+public class HeartInspector : Editor{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        var myTarget = (Heart) target;
+        
+        myTarget.random = EditorGUILayout.Toggle("Is Random ?", myTarget.random);
+            
+        if (myTarget.random)
+        {
+            myTarget.minHealth = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Minimum Health",myTarget.minHealth), 0, ushort.MaxValue);
+            myTarget.maxHealth = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Maximum Health",myTarget.maxHealth), 0, ushort.MaxValue);
+        }
+        else
+        {
+            myTarget.health = (ushort) Mathf.Clamp(EditorGUILayout.IntField("Health",myTarget.health), 0, ushort.MaxValue);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        if (!GUI.changed) return;
+        EditorUtility.SetDirty(myTarget);
+        EditorSceneManager.MarkSceneDirty(myTarget.gameObject.scene);
+    }
+} 
