d2b31b6943a2c5689eb801e6d0257676c633b196
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-15 22:11:10 +0200

    - Adding Heart Item

---
+++ Assets/Scripts/Items/Heart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Items
+{
+    public class Heart : MonoBehaviour
+    {
+        public ushort health;
+        public bool random;
+        public ushort minHealth;
+        public ushort maxHealth;
+        
+        private void Awake()
+        {
+            if (random)
+            {
+                health = (ushort) Random.Range(minHealth, maxHealth);
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+            var ply = other.GetComponent<Player>();
+            if (ply.health + health >= 100.0f) return;
+            ply.health += health;
+            Destroy(gameObject);
+        }
+    }
+}
