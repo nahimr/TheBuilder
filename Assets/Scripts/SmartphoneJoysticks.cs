ba73b60c757aa3ad24fef646c51304a24dfa3891
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-15 22:11:59 +0200

    - Implementing SmartphoneJoysticks class with static instance

---
+++ Assets/Scripts/SmartphoneJoysticks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+public class SmartphoneJoysticks : MonoBehaviour
+{
+    public FixedJoystick moveStick;
+    public JoystickButton jumpButton;
+    public JoystickButton takeButton;
+    public JoystickButton fireButton;
+    public JoystickButton specialButton;
+    public static SmartphoneJoysticks Instance { get; private set; }
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
