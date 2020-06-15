dc3b63249e8e52c1743ebac11010a87130764ae6
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-15 22:15:14 +0200

    - Implementing UI_Ingame class with static instance

---
+++ Assets/Scripts/UI/UI_InGame.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UI_InGame : MonoBehaviour
+    {
+        [Header("HUD")] 
+        public GameObject hud;
+        public Slider healthBar;
+        public Slider staminaBar;
+       
+        public Text scoreText;
+        public Text timerText;
+        public Text ammoText;
+        public Button pauseButton;
+
+        [Header("Pause Menu")] 
+        public GameObject pauseMenu;
+        public Button resumeButton;
+        public Button retryButton;
+        public Button exitGameButton;
+    
+        [Header("Game Finished")] 
+        public GameObject gameFinished;
+        public Text finalScoreText;
+        public Text finalTimeText;
+        public Text resultText;
+        public GameObject ad;
+        public Button finalRetryButton;
+        public Button finalSettingsButton;
+        public Button finalExitButton;
+
+        [Header("Mount")] 
+        public Slider jetpackBar;
+        public Image jetpackBarImage;
+        
+        [Header("Other")] 
+        public GameObject joysticks;
+        public static UI_InGame Instance { get; private set; }
+
+        
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
