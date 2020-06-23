180aef4f0b1a8d197de5fa8e280d32e9fefde8ae
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-23 21:55:14 +0200

    - Adding LevelGenerator.cs tool to generate level without making scene

---
+++ Assets/Scripts/UI/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace UI
+{
+    [System.Serializable]
+    public class LevelData
+    {
+        public ushort id;
+        public ushort numberOfBricksToWin;
+        public float timeToComplete;
+    
+    }
+    public class LevelGenerator : MonoBehaviour
+    {
+        public LevelData[] levels;
+    }
+}
