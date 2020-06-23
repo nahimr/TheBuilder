719ad45fd14685e70db9d04ab00d7a35dbe9fe0d
Nahim Rouidjali <nahim.rouidjali@gmail.com>
2020-06-23 21:54:40 +0200

    - Adding JsonHelper.cs library

---
+++ Assets/Scripts/Backend/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class JsonHelper
+{
+    //Usage:
+    //YouObject[] objects = JsonHelper.getJsonArray<YouObject> (jsonString);
+    public static T[] GetJsonArray<T>(string json)
+    {
+        //var newJson = "{ \"array\": " + json + "}";
+        var wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.array;
+    }
+
+    //Usage:
+    //string jsonString = JsonHelper.arrayToJson<YouObject>(objects);
+    public static string ArrayToJson<T>(T[] array)
+    {
+        var wrapper = new Wrapper<T> {array = array};
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
